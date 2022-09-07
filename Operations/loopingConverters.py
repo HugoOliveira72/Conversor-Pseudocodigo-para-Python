4c9c02c510ce41d3faef5245229a448c1b75940d
hugooliveira72 <hugo72900@gmail.com>
2022-09-07 11:58:23 -0300

    BugFix: Converter multiplos Paras

--- Operations/loopingConverters.py
+++ Operations/loopingConverters.py
@@ -9,36 +9,39 @@ class loopingConverters:
         self.command = "para "
         self.textLines = readFileLines("Docs/Files/","entrada")
         self.forBase = "for @contador in range(@de, @ate, @passo):"
-        self.text = ""
+        self.forLines = []
 
     def forConverter(self):
         lineCounter = 0
+        forAtrCounter = 0
         forCounter = 0
+        indexLineFor = []
 
         for line in self.textLines:
-            if line.find("para") != -1:
-                self.text = removeNBar(line)
-                break
-            else:
-                lineCounter += 1
-                return
+            if line.find("para ") != -1:
+                self.forLines.append(removeNBar(line))
+                indexLineFor.append(lineCounter)
+            lineCounter += 1
             
         For = Para()
-        for parameter in For.atributes:
-            forCounter += 1
-    
-            index = self.text.find(parameter) + len(parameter)
+        for actualForLine in self.forLines:
+            for parameter in For.atributes:
+                forAtrCounter += 1
+        
+                index = actualForLine.find(parameter) + len(parameter)
 
-            if forCounter < For.ForLen:
-                actual_value = For.atributes[forCounter]
-                value = self.text.find(actual_value[:-1])
-                variable = self.text[index+1:value-1]
-            
-            if forCounter == 1:
-                self.forBase = self.forBase.replace(f"@contador",variable)
-            else:
-                self.forBase = self.forBase.replace(f"@{parameter}",variable)
+                if forAtrCounter < For.ForLen:
+                    actual_value = For.atributes[forAtrCounter]
+                    value = actualForLine.find(actual_value[:-1])
+                    variable = actualForLine[index+1:value-1]
                 
-        self.textLines[lineCounter] = self.forBase+"\n"
+                if forAtrCounter == 1:
+                    self.forBase = self.forBase.replace(f"@contador",variable)
+                else:
+                    self.forBase = self.forBase.replace(f"@{parameter}",variable)
+
+                self.textLines[indexLineFor[forCounter]] = self.forBase+"\n"
+                    
+            forCounter += 1
         return convertToText(self.textLines)
     
