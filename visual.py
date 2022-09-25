41ba418b7ac8ec57e48912913902249f38a994a3
hugooliveira72 <hugo72900@gmail.com>
2022-09-25 17:07:45 -0300

    Ajuste parte visual .Mudanca das cores

--- visual.py
+++ visual.py
@@ -1,40 +1,58 @@
+from cgitb import text
 from tkinter import *
 from tkinter.messagebox import showinfo
-
-# from matplotlib.pyplot import text
+from tkinter.tix import LabelEntry
 from Operations.fileActions import *
 from main import Main
 
+class Funcoes():
+    def converter(self):
+        self.entradaTexto2.delete("1.0", "end")
+        writeFile(self.entradaTexto1.get("1.0", 'end-1c'), "Docs/Files", "entrada")
+        Main()
+        outputFile = readFile("Docs/Files", "saida")
+        self.entradaTexto2.insert(INSERT, outputFile)
+        showinfo(title='Conversor', message='Convertido')
 
-def Converter_Clicked():
-    texto2.delete("1.0","end")
-    writeFile(texto1.get("1.0",'end-1c'),"Docs/Files","entrada")
-    mainClass = Main()
-    outputFile = readFile("Docs/Files","saida")
-    texto2.insert(INSERT, outputFile)
-    showinfo(title='Conversor',message='Convertido')
+root = Tk()
 
-janela = Tk()
-janela.title("Conversor pseudocodigo")
-janela.configure(bg="#b8c4ed")
-janela.geometry("1366x768")
+class Aplicacao(Funcoes):
+    def __init__(self):
+        self.root = root
+        self.tela()
+        self.frames()
+        self.entradas()
+        self.botoes()
+        self.root.mainloop()
 
-tituloJanela = Label(janela,text='Bem vindo ao conversor de pseudcódigo!',bg="#b8c4ed",font="Arial",fg="Blue")
-tituloJanela.grid(row=0,column=6)
+    def tela(self):
+        self.root.title("Conversor pseudocodigo")
+        self.root.configure(background="#EEE4AB")
+        self.root.geometry("1366x768")
+        self.root.resizable(True, True)
+        self.root.maxsize = "1920x1080"
+        self.root.minsizev = "720x480"
 
-vazio = Label(janela,text='                ',bg="#b8c4ed",font="Arial",fg="Blue")
-vazio.grid(row=0,column=1)
-titulotexto1 = Label(janela,text='Pseudocódigo',bg="#b8c4ed",font="Arial",fg="Blue")
-titulotexto1.grid(row=2,column=2)
-texto1 = Text(janela,height=30,width=50, font="Arial")
-texto1.grid(row=3,column=2)
+    def frames(self):
+        self.frame1 = Frame(self.root, bd=4, bg="#99C4C8",
+                            highlightbackground="#68A7AD", highlightthickness=3)
+        self.frame1.place(relx=0.14, rely=0.13, relwidth=0.3, relheight=0.68)
 
-titulotexto2 = Label(janela,text='Python',bg="#b8c4ed",font="Arial",fg="Blue")
-titulotexto2.grid(row=2,column=12)
-texto2 = Text(janela,height=30,width=50, font="Arial")
-texto2.grid(row=3,column=12)
+        self.frame2 = Frame(self.root, bd=4, bg="#99C4C8",
+                            highlightbackground="#68A7AD", highlightthickness=3)
+        self.frame2.place(relx=0.5, rely=0.13, relwidth=0.3, relheight=0.68)
 
-botao = Button(janela,text="Converter", bg="Blue",fg="white",height=3,width=10,command=Converter_Clicked)
-botao.grid(row=5,column=6)
+    def entradas(self):
+        self.entradaTexto1 = Text(self.frame1, font="Courier")
+        self.entradaTexto1.place(
+            relx=0.001, rely=0.001, relwidth=0.999, relheight=0.999)
 
-janela.mainloop()
+        self.entradaTexto2 = Text(self.frame2, font="Courier")
+        self.entradaTexto2.place(
+            relx=0.001, rely=0.001, relwidth=0.999, relheight=0.999)
+
+    def botoes(self):
+        self.botaoConverter = Button(root, text="Converter", command=self.converter)
+        self.botaoConverter.place(relx=0.46, rely=0.87, relwidth=0.05, relheight=0.05)
+
+Aplicacao()
