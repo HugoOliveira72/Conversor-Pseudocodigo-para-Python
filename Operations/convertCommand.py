366c7abf3ebd4d0825235dbf7fa646d08477a498
hugooliveira72 <hugo72900@gmail.com>
2022-10-20 22:23:54 -0300

    Implementação digitar palavras reservadas dentro do escreva.

--- Operations/convertCommand.py
+++ Operations/convertCommand.py
@@ -3,6 +3,7 @@ from distutils import text_file
 from queue import Empty
 from Operations.fileActions import *
 from Operations.loopingConverters import LoopingConverter
+from Operations.marks import fixMarks
 from Operations.stringExtensions import *
 
 def convertWord():
@@ -19,13 +20,17 @@ def convertWord():
         text = readFile("Docs/Files","entrada")
 
     textLines = readFileLines("Docs/Files/","entrada")
+    currentParameter = ''
 
     for j in range(0,len(textLines)):
+        # Aplicar regra das aspas
+        textLines[j], currentParameter = fixMarks(textLines[j])
         for i in range(0, len(inputLines)):
             find = removeNBar(inputLines[i])
             change = removeNBar(outputLines[i])
             textLines[j] = replaceFile(textLines[j], find, change)
 
+        textLines[j] = replaceFile(textLines[j],"@parametro",currentParameter)
 
     writeStringListExitFile(textLines)
 
--- Operations/marks.py
+++ Operations/marks.py
@@ -1,18 +1,10 @@
-def fixMarks(texto):
-
-
-    frase = "escreval(\"escreval mundo odeia o crhis\")"    
-    lines = []
-
-    for lines in texto:
-        lines.append(lines)
-
+def fixMarks(frase):
     frases = []
     nova_frase = ""
     achou_aspas = False
     qt_aspas = 0
 
-    for i in texto:
+    for i in frase:
         if (i == '\"' and achou_aspas == False and qt_aspas == 0):
             nova_frase += '\"'
             achou_aspas = True
@@ -26,20 +18,4 @@ def fixMarks(texto):
         elif (achou_aspas and qt_aspas == 1):
             nova_frase += i
 
-    print("frase sem tratativas")
-    print(frase)
-    print("frase que ira substituir a palavra  @parametro")
-    print(nova_frase)
-    print("convertendo codigo")
-    frase = frase.replace("escreval", "printf")
-    print("depois de converter")
-    print(frase)
-    print("substituindo parametro")
-    frase = frase.replace("@parametro", nova_frase)
-    print("frase depois de convertida")
-    print(frase)
-
-
-    frases.append(nova_frase)
-    index1 = len(frases)
-    print(f"@parametro{(index1 - 1)}")
+    return frase, nova_frase
