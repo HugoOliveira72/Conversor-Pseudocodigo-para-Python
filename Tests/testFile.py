b1ff3af17f639db2b189069c99d6fabd8bb3823e
hugooliveira72 <hugo72900@gmail.com>
2022-11-08 00:59:40 -0300

    Inteiro hotfix

--- Tests/testFile.py
+++ Tests/testFile.py
@@ -1,11 +1,31 @@
 # algoritmo  "Hello World!"
-import math
+# import math
 
 
-c = 0.0
-b = 0.0
+# c = 0.0
+# b = 0.0
 
-print("Hello World!")
-b = 49
-c = math.issqrt ( b )
-print("c:",c)
+# print("Hello World!")
+# b = 49
+# c = math.issqrt ( b )
+# print("c:",c)
+
+# algoritmo  "1007"
+
+
+n1 = 0
+n2 = 0
+n3 = 0
+n4 = 0
+diferenca = 0
+
+
+n1 = input()
+n2 = input()
+n3 = input()
+n4 = input()
+diferenca = (n1 * n2) - (n3 * n4)
+print("DIFERENCA = ",diferenca)
+
+if n1 % n2 == 0:
+    print('a')
